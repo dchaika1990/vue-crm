93ead78f00924edb63b0ad9e4fb57fa9515d92c1
Dima <dchaika1990@gmail.com>
2021-04-22 22:42:42 +0300

    add dropdown

--- src/components/app/FixedBtn.vue
+++ src/components/app/FixedBtn.vue
@@ -1,8 +1,11 @@
 <template>
 	<div class="fixed-action-btn">
-		<a class="btn-floating btn-large blue" href="#">
+		<router-link
+			class="btn-floating btn-large blue"
+			to="/record"
+		>
 			<i class="large material-icons">add</i>
-		</a>
+		</router-link>
 	</div>
 </template>
 
